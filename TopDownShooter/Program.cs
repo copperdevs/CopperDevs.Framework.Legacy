7f877dc3a59d9e69dbe27aec12a2b5040da4f672
copper <79734708+OlympianGames@users.noreply.github.com>
2024-05-02 10:18:39 -0500

    top down shooter start

---
+++ TopDownShooter/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace TopDownShooter;
+
+public static class Program
+{
+    public static void Main()
+    {
+        var engineSettings = new EngineSettings()
+        {
+            DisableDevTools = false,
+            EnableDevToolsAtStart = true,
+            TargetFps = 144,
+            WindowTitle = "Copper Framework - Top Down Shooter"
+        };
+        
+        var engine = new Engine(engineSettings);
+        
+        engine.Run();
+    }
+}
